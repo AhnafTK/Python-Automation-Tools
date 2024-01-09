452c478e841f3cd23fc23bd2dff0c99a0d4b4c3b
Ahnaf Kamal <ahnaf.kamal@outlook.com>
2024-01-09 11:14:14 -0500

    added unsorted support & removed watchdog functionalities temp

--- FolderSorter/main.py
+++ FolderSorter/main.py
@@ -1,76 +1,56 @@
-import os 
-import shutil
-import re
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-import time
+import os
+#import re
+# from watchdog.observers import Observer
+# from watchdog.events import FileSystemEventHandler
+# import time
 
-watchPath = "C:\\Users\\ahnaf\\Downloads"
-
-
-trackList = {
-        ".jpg$" : watchPath+"\\Images",
-        ".png$" : watchPath+"\\Images",
-        ".webm$": watchPath+"\\Webms",
-        ".mp4$": watchPath+"\\Vids",
-        ".pdf$": watchPath+"\\Pdfs",
-        ".rbxl$": watchPath+"\\RobloxFiles",
-        ".rbxm$": watchPath+"\\RobloxFiles\\Rbmxs",
-        ".docx$": watchPath+"\\Documents",
-        ".zip$" : watchPath+"\\Zips",
-        ".txt$" : watchPath+"\\Text Files",
-        ".exe$" : watchPath+"\\Executables",
-        ".wav$" : watchPath+"\\Wavs",       
+watch_path = "C:\\Users\\ahnaf\\Downloads\\test2"
+# where the unsorted files go (folders aren't sorted)
+unsorted = watch_path+"\\Unsorted"
+# dictionary containing the targetted formats and their dest
+track_list = {
+    ".jpg": watch_path+"\\Images",
+    ".png": watch_path+"\\Images",
+    ".webm": watch_path+"\\Webms",
+    ".mp4": watch_path+"\\Vids",
+    ".pdf": watch_path+"\\Pdfs",
+    ".docx": watch_path+"\\Documents",
+    ".zip": watch_path+"\\Zips",
+    ".txt": watch_path+"\\Text Files",
+    ".exe": watch_path+"\\Executables",
+    ".wav": watch_path+"\\Wavs",
+    ".mov": watch_path+"\\movs",
 }
 
-unsorted = watchPath+"\\Unsorted"
+for file in os.listdir(watch_path): 
+    # print(file)
 
-class SortFolder(FileSystemEventHandler):
-    
-    def on_modified(self, event):
-        for file in os.listdir(watchPath): # for each of the items in the dirclass
-            found = False
-            print(file + " FOUND? " + str(found))
+    # get the extension of the file
+    # returns tuple, index 1 contains extension
+    file_extension = os.path.splitext(file)[1]
 
-            for extension, location in trackList.items():   # compare the extension of the file against tracklist
-                if found : break 
-                print("trying" + str(extension))
-                
-                if re.search(extension, file):  # if there is a match
-                    if not os.path.exists(location): # check if the folder where it's meant to go doesnt exists
-                        try:
-                            print("making dir " + location)
-                            os.makedirs(location)
-                        except OSError as error:
-                            print(error)
+    if file_extension.lower() in track_list:
+        # it exists
+        dest_path = track_list[file_extension.lower()]
+        # check if the folder where it's meant to go doesnt exists
+        if not os.path.exists(dest_path):
+            try:
+                print("making dir " + dest_path)
+                os.makedirs(dest_path)
+            except OSError as error:
+                print(error)
 
-                    print(watchPath+"\\"+file+" -> " + location + "\\" + file)
-                    #os.rename(watchPath+"\\"+file,location+"\\"+file)
-                    try:
-                        os.replace(watchPath+"\\"+file,location+"\\"+file)
-                        found = True
-                    except OSError as error:
-                        print(error)
+        try:
+            os.replace(watch_path+"\\"+file, dest_path+"\\"+file)
+        except OSError as error:
+            print(error)
 
-            print("UNSORTED")
-            if not os.path.exists(unsorted): # check if the folder where it's meant to go doesnt exists
-                try:
-                print("making dir " + unsorted)
-                    os.makedirs(unsorted)
-                    found = True
-                except OSError as error:
-                    print(error)
-
-
-observer = Observer()
-event_handler = SortFolder()
-observer.schedule(event_handler, watchPath, recursive = False)
-observer.start()
-
-try:
-    while True:
-        time.sleep(10)
-except KeyboardInterrupt:
-    observer.stop()
-observer.join()
-
+    else:
+        # check if its a folder
+        if os.path.isfile(watch_path+"\\"+file):
+            print(watch_path+"\\"+file + " iS FILE")
+            dest_path = unsorted
+            try:
+                os.replace(watch_path+"\\"+file, unsorted+"\\"+file)
+            except OSError as error:
+                print(error)
